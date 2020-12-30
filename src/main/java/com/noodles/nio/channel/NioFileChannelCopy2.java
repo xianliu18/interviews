25055783057540d63c351f41b383974ebc931066
liuxian2 <liuxian2@lenovo.com>
2020-12-30 10:50:25 +0800

    remove unused import

--- src/main/java/com/noodles/nio/channel/NioFileChannelCopy2.java
+++ src/main/java/com/noodles/nio/channel/NioFileChannelCopy2.java
@@ -1,10 +1,8 @@
 package com.noodles.nio.channel;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
