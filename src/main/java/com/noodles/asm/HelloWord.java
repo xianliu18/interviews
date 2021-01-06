63ea18c81bc4d42aac9f4293ecc54f27bf47eab7
noodlescnliu <noodlescnliu@gmail.com>
2021-01-06 08:47:02 +0800

    字节码编程 1

---
+++ src/main/java/com/noodles/asm/HelloWord.java
@@ -0,0 +1,12 @@
+package com.noodles.asm;
+
+/**
+ * @Description: 测试用例
+ * @Author: noodles
+ * @create: 2021-01-06 08:39
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
