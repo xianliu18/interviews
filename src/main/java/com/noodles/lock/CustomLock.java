e7f8c9f0d98df394691f4040832fcbd4bfc1168a
liuxian2 <liuxian2@lenovo.com>
2021-02-08 11:19:54 +0800

    通过AQS自定义锁

---
+++ src/main/java/com/noodles/lock/CustomLock.java
@@ -0,0 +1,41 @@
+package com.noodles.lock;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @ClassName CustomLock
+ * @Description 使用AQS自定义锁
+ * @Author noodles
+ * @Date 2021/2/8 11:13
+ */
+public class CustomLock {
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+
+        @Override
+        protected boolean tryAcquire(int arg) {
+            return compareAndSetState(0, 1);
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+    }
+
+    private Sync sync = new Sync();
+
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    public void unlock() {
+        sync.release(1);
+    }
+}
---
+++ src/main/java/com/noodles/lock/CustomLockTest.java
@@ -0,0 +1,39 @@
+package com.noodles.lock;
+
+/**
+ * @ClassName CustomLockTest
+ * @Description 自定义锁测试
+ * @Author noodles
+ * @Date 2021/2/8 11:17
+ */
+public class CustomLockTest {
+
+    static int count = 0;
+    static CustomLock customLock = new CustomLock();
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    customLock.lock();
+                    for (int i = 0; i < 10000; i++) {
+                        count++;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    customLock.unlock();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(count);
+    }
+}
