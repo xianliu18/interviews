33e1702a8c2167e7eb008f7f67b7bb3a0eeb57e6
liuxian2 <liuxian2@lenovo.com>
2021-03-16 13:53:34 +0800

    共享锁/独占锁

---
+++ src/main/java/com/noodles/lock/ReadWriteLockDemo.java
@@ -0,0 +1,74 @@
+package com.noodles.lock;
+
+import com.noodles.netty.codec3_protostuff.codec.ObjDecoder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @ClassName ReadWriteLockDemo
+ * @Description 共享锁/独占锁
+ * @Author noodles
+ * @Date 2021/3/16 11:34
+ */
+
+class MyCache {
+    private volatile Map<String, Object> map = new HashMap<>();
+    private ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+
+    public void put(String key, Object value) {
+        rwLock.writeLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t 正在写入: " + key);
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + "\t 写入完成。");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            rwLock.writeLock().unlock();
+        }
+    }
+
+    public void get(String key) {
+        rwLock.readLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t 正在读取...");
+            // 暂停线程
+            TimeUnit.MILLISECONDS.sleep(300);
+            Object result = map.get(key);
+            System.out.println(Thread.currentThread().getName() + "\t 读取完成：" + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            rwLock.readLock().unlock();
+        }
+    }
+}
+
+public class ReadWriteLockDemo {
+    public static void main(String[] args) {
+        MyCache myCache = new MyCache();
+
+        for (int i = 1; i <= 5; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                myCache.put(tempInt + "", tempInt + "");
+            }, String.valueOf(i)).start();
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                myCache.get(tempInt + "");
+            }, String.valueOf(i)).start();
+        }
+
+    }
+}
