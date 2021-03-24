51f1e08192e61fcd007e1e45ea44430db51f878e
noodlescnliu <noodlescnliu@gmail.com>
2021-03-24 21:32:35 +0800

    MaxDepthBinaryTree

--- algorithm/src/main/java/com/noodles/leetcode/MaxDepthBinaryTree.java
+++ algorithm/src/main/java/com/noodles/leetcode/MaxDepthBinaryTree.java
@@ -2,6 +2,9 @@ package com.noodles.leetcode;
 
 import com.noodles.base.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @ClassName MaxDepthBinaryTree
  * @Description  Maximum Depth of Binary Tree
@@ -12,15 +15,56 @@ import com.noodles.base.TreeNode;
  */
 public class MaxDepthBinaryTree {
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode nodeA = new TreeNode(9);
+        TreeNode nodeB = new TreeNode(20);
+        TreeNode nodeC = new TreeNode(15);
+        TreeNode nodeD = new TreeNode(7);
+        TreeNode nodeF = new TreeNode(18);
+        TreeNode nodeG = new TreeNode(21);
+        root.left = nodeA;
+        root.right = nodeB;
+        nodeA.right = nodeF;
+        nodeF.left = nodeG;
+        nodeB.left = nodeC;
+        nodeB.right = nodeD;
 
+//        System.out.println("Recur: " + maxDepthRecur(root));
+
+        System.out.println("BFS: " + maxDepthBFS(root));
     }
 
 
-    public static int maxDepth(TreeNode root) {
+    public static int maxDepthRecur(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
 
+        int leftMax = root.left == null ? 0 : maxDepthRecur(root.left);
+        int rightMax = root.right == null ? 0 : maxDepthRecur(root.right);
+        return 1 + Math.max(leftMax, rightMax);
+    }
 
-
-        // TODO: 如何做？？
-        return 0;
+    public static int maxDepthBFS(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            count++;
+        }
+        return count;
     }
 }
