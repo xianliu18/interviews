4df5f9340e11a3ecefc45b73af8303ea8d0d1f02
noodlescnliu <noodlescnliu@gmail.com>
2021-05-15 23:54:18 +0800

    增加注释

--- dubbo_consumer/src/main/java/com/noodles/ConsumerApplication.java
+++ dubbo_consumer/src/main/java/com/noodles/ConsumerApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @ClassName ConsumerApplication
  * @Description 主启动类
+ *      1, 导入 dubbo-starter, 在 application.properties 设置属性
+ *      2, 使用 @Service 暴露服务, 使用 @Reference 引用服务
+ *      3, @EnableDubbo 开启基于注解的 dubbo 功能
  * @Author noodles
  * @Date 2021/3/12 16:05
  */
